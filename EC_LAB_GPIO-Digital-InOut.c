6299de6e345b4642f3b99bca4d03445f16925445
DABINHEO <113574284+DABINHEO@users.noreply.github.com>
2022-10-02 14:59:17 +0900

    Update EC_LAB_GPIO-Digital-InOut.c

--- EC_LAB_GPIO-Digital-InOut.c
+++ EC_LAB_GPIO-Digital-InOut.c
@@ -40,3 +40,56 @@ void setup(void)
 	LED_init();
 	
 }
+/**
+******************************************************************************
+* @author	HEO DABIN
+* @Mod		2020.10.02
+* @brief	Embedded Controller:  LAB Digital In/Out
+*					 - Toggle LED LD2 by Button B1 pressing
+* 
+******************************************************************************
+*/
+
+#include "stm32f4xx.h"
+#include "ecRCC.h"
+#include "ecGPIO.h"
+
+#define LED_PIN 	5
+#define BUTTON_PIN 13
+
+void setup(void);
+	
+int main(void) { 
+	int num = 0;
+	int State[4][4] = {
+	{1,0,0,0},
+	{0,1,0,0},
+	{0,0,1,0},
+	{0,0,0,1}
+	};	
+	// Initialiization --------------------------------------------------------
+	setup();
+	// Inifinite Loop ----------------------------------------------------------
+	while(1){
+		GPIO_write(GPIOA, 5, State[num][0]);
+		GPIO_write(GPIOA, 6, State[num][1]);
+		GPIO_write(GPIOA, 7, State[num][2]);
+		GPIO_write(GPIOB, 6, State[num][3]);
+		if(GPIO_read(GPIOC, BUTTON_PIN) == 0)	{
+			num++;
+			if (num > 3) num = 0;
+			for(int i = 0; i < 500000;i++){}
+		}
+	}
+}
+
+
+// Initialiization 
+void setup(void)
+{
+	RCC_HSI_init();	
+	GPIO_init(GPIOC, BUTTON_PIN, INPUT);
+	GPIO_pupd(GPIOC, BUTTON_PIN, EC_PU);
+	LED_init();
+	
+}
